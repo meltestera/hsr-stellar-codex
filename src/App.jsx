560db641f3ee9c68d495895ce2c62ef5d1599af6
meltestera <meltestera@gmail.com>
2023-11-26 20:12:39 +0800

    added react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import "./App.css";
+
+// import router
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+// import components
+import Root from "./components/Root";
+import CharacterDetails from "./components/CharacterDetails";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        path: ":name",
+        element: <CharacterDetails />,
+      },
+    ],
+  },
+]);
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 };
 
 export default App;
