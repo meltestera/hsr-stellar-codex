7b75ae6dfb5ee1627f913a926b22efb333a92d6b
meltestera <meltestera@gmail.com>
2023-11-26 19:40:38 +0800

    initial commit

---
+++ src/assets/characters.jsx
@@ -0,0 +1,436 @@
+// 4-star character icons
+
+import CharacterIcon1 from "./characters/4stars/arlan/Character_Arlan_Icon.webp";
+import CharacterIcon2 from "./characters/4stars/asta/Character_Asta_Icon.webp";
+import CharacterIcon3 from "./characters/4stars/danheng/Character_Danheng_Icon.webp";
+import CharacterIcon4 from "./characters/4stars/guinaifen/Character_Guinaifen_Icon.webp";
+import CharacterIcon5 from "./characters/4stars/herta/Character_Herta_Icon.webp";
+import CharacterIcon6 from "./characters/4stars/hook/Character_Hook_Icon.webp";
+import CharacterIcon7 from "./characters/4stars/luka/Character_Luka_Icon.webp";
+import CharacterIcon8 from "./characters/4stars/lynx/Character_Lynx_Icon.webp";
+import CharacterIcon9 from "./characters/4stars/march7th/Character_March_7th_Icon.webp";
+import CharacterIcon10 from "./characters/4stars/natasha/Character_Natasha_Icon.webp";
+import CharacterIcon11 from "./characters/4stars/pela/Character_Pela_Icon.webp";
+import CharacterIcon12 from "./characters/4stars/qingque/Character_Qingque_Icon.webp";
+import CharacterIcon13 from "./characters/4stars/sampo/Character_Sampo_Icon.webp";
+import CharacterIcon14 from "./characters/4stars/serval/Character_Serval_Icon.webp";
+import CharacterIcon15 from "./characters/4stars/sushang/Character_Sushang_Icon.webp";
+import CharacterIcon16 from "./characters/4stars/tingyun/Character_Tingyun_Icon.webp";
+import CharacterIcon17 from "./characters/4stars/yukong/Character_Yukong_Icon.webp";
+
+// 5-star character icons
+
+import CharacterIcon18 from "./characters/5stars/bailu/Character_Bailu_Icon.webp";
+import CharacterIcon19 from "./characters/5stars/blade/Character_Blade_Icon.webp";
+import CharacterIcon20 from "./characters/5stars/bronya/Character_Bronya_Icon.webp";
+import CharacterIcon21 from "./characters/5stars/clara/Character_Clara_Icon.webp";
+import CharacterIcon22 from "./characters/5stars/fuxuan/Character_Fu_Xuan_Icon.webp";
+import CharacterIcon23 from "./characters/5stars/gepard/Character_Gepard_Icon.webp";
+import CharacterIcon24 from "./characters/5stars/himeko/Character_Himeko_Icon.webp";
+import CharacterIcon25 from "./characters/5stars/imbibitorlunae/Character_Imbibitor_Lunae_Icon.webp";
+import CharacterIcon26 from "./characters/5stars/jingliu/Character_Jingliu_Icon.webp";
+import CharacterIcon27 from "./characters/5stars/jingyuan/Character_Jing_Yuan_Icon.webp";
+import CharacterIcon28 from "./characters/5stars/kafka/Character_Kafka_Icon.webp";
+import CharacterIcon29 from "./characters/5stars/luocha/Character_Luocha_Icon.webp";
+import CharacterIcon30 from "./characters/5stars/seele/Character_Seele_Icon.webp";
+import CharacterIcon31 from "./characters/5stars/silverwolf/Character_Silver_Wolf_Icon.webp";
+import CharacterIcon32 from "./characters/5stars/topaz/Character_Topaz_Icon.webp";
+import CharacterIcon33 from "./characters/5stars/trailblazer/female/physical/Character_Trailblazer_Female_Destruction_Icon.webp";
+import CharacterIcon34 from "./characters/5stars/trailblazer/female/fire/Character_Trailblazer_Female_Preservation_Icon.webp";
+import CharacterIcon35 from "./characters/5stars/trailblazer/male/physical/Character_Trailblazer_Male_Destruction_Icon.webp";
+import CharacterIcon36 from "./characters/5stars/trailblazer/male/fire/Character_Trailblazer_Male_Preservation_Icon.webp";
+import CharacterIcon37 from "./characters/5stars/welt/Character_Welt_Icon.webp";
+import CharacterIcon38 from "./characters/5stars/yanqing/Character_Yanqing_Icon.webp";
+
+// character type icons
+
+import Fire from "./Class_Type_Fire.webp";
+import Ice from "./Class_Type_Ice.webp";
+import Imaginary from "./Class_Type_Imaginary.webp";
+import Lightning from "./Class_Type_Lightning.webp";
+import Physical from "./Class_Type_Physical.webp";
+import Quantum from "./Class_Type_Quantum.webp";
+import Wind from "./Class_Type_Wind.webp";
+
+export const characters = [
+  {
+    name: "Arlan",
+    rarity: 4,
+    id: 1,
+    src: CharacterIcon1,
+    type: Lightning,
+    path: "Destruction",
+    alt: "Character_Arlan_Icon",
+    typeAlt: "Class_Type_Lightning",
+  },
+  {
+    name: "Asta",
+    rarity: 4,
+    id: 2,
+    src: CharacterIcon2,
+    type: Fire,
+    path: "Harmony",
+    alt: "Character_Asta_Icon",
+    typeAlt: "Class_Type_Fire",
+  },
+  {
+    name: "Dan Heng",
+    rarity: 4,
+    id: 3,
+    src: CharacterIcon3,
+    type: Wind,
+    path: "Hunt",
+    alt: "Character_Danheng_Icon",
+    typeAlt: "Class_Type_Wind",
+  },
+  {
+    name: "Guinaifen",
+    rarity: 4,
+    id: 4,
+    src: CharacterIcon4,
+    type: Fire,
+    path: "Nihility",
+    alt: "Character_Guinaifen_Icon",
+    typeAlt: "Class_Type_Fire",
+  },
+  {
+    name: "Herta",
+    rarity: 4,
+    id: 5,
+    src: CharacterIcon5,
+    type: Ice,
+    path: "Erudition",
+    alt: "Character_Herta_Icon",
+    typeAlt: "Class_Type_Ice",
+  },
+  {
+    name: "Hook",
+    rarity: 4,
+    id: 6,
+    src: CharacterIcon6,
+    type: Fire,
+    path: "Destruction",
+    alt: "Character_Hook_Icon",
+    typeAlt: "Class_Type_Fire",
+  },
+  {
+    name: "Luka",
+    rarity: 4,
+    id: 7,
+    src: CharacterIcon7,
+    type: Physical,
+    path: "Nihility",
+    alt: "Character_Luka_Icon",
+    typeAlt: "Class_Type_Physical",
+  },
+  {
+    name: "Lynx",
+    rarity: 4,
+    id: 8,
+    src: CharacterIcon8,
+    type: Quantum,
+    path: "Abundance",
+    alt: "Character_Lynx_Icon",
+    typeAlt: "Class_Type_Quantum",
+  },
+  {
+    name: "March 7th",
+    rarity: 4,
+    id: 9,
+    src: CharacterIcon9,
+    type: Ice,
+    path: "Preservation",
+    alt: "Character_March_7th_Icon",
+    typeAlt: "Class_Type_Ice",
+  },
+  {
+    name: "Natasha",
+    rarity: 4,
+    id: 10,
+    src: CharacterIcon10,
+    type: Physical,
+    path: "Abundance",
+    alt: "Character_Natasha_Icon",
+    typeAlt: "Class_Type_Physical",
+  },
+  {
+    name: "Pela",
+    rarity: 4,
+    id: 11,
+    src: CharacterIcon11,
+    type: Ice,
+    path: "Nihility",
+    alt: "Character_Pela_Icon",
+    typeAlt: "Class_Type_Ice",
+  },
+  {
+    name: "Qingque",
+    rarity: 4,
+    id: 12,
+    src: CharacterIcon12,
+    type: Quantum,
+    path: "Erudition",
+    alt: "Character_Qingque_Icon",
+    typeAlt: "Class_Type_Quantum",
+  },
+  {
+    name: "Sampo",
+    rarity: 4,
+    id: 13,
+    src: CharacterIcon13,
+    type: Wind,
+    path: "Nihility",
+    alt: "Character_Sampo_Icon",
+    typeAlt: "Class_Type_Wind",
+  },
+  {
+    name: "Serval",
+    rarity: 4,
+    id: 14,
+    src: CharacterIcon14,
+    type: Lightning,
+    path: "Erudition",
+    alt: "Character_Serval_Icon",
+    typeAlt: "Class_Type_Lightning",
+  },
+  {
+    name: "Sushang",
+    rarity: 4,
+    id: 15,
+    src: CharacterIcon15,
+    type: Physical,
+    path: "Hunt",
+    alt: "Character_Sushang_Icon",
+    typeAlt: "Class_Type_Physical",
+  },
+  {
+    name: "Tingyun",
+    rarity: 4,
+    id: 16,
+    src: CharacterIcon16,
+    type: Lightning,
+    path: "Harmony",
+    alt: "Character_Tingyun_Icon",
+    typeAlt: "Class_Type_Lightning",
+  },
+  {
+    name: "Yukong",
+    rarity: 4,
+    id: 17,
+    src: CharacterIcon17,
+    type: Imaginary,
+    path: "Harmony",
+    alt: "Character_Yukong_Icon",
+    typeAlt: "Class_Type_Imaginary",
+  },
+  {
+    name: "Bailu",
+    rarity: 5,
+    id: 18,
+    src: CharacterIcon18,
+    type: Lightning,
+    path: "Abundance",
+    alt: "Character_Bailu_Icon",
+    typeAlt: "Class_Type_Lightning",
+  },
+  {
+    name: "Blade",
+    rarity: 5,
+    id: 19,
+    src: CharacterIcon19,
+    type: Wind,
+    path: "Destruction",
+    alt: "Character_Blade_Icon",
+    typeAlt: "Class_Type_Wind",
+  },
+  {
+    name: "Bronya",
+    rarity: 5,
+    id: 20,
+    src: CharacterIcon20,
+    type: Wind,
+    path: "Harmony",
+    alt: "Character_Bronya_Icon",
+    typeAlt: "Class_Type_Wind",
+  },
+  {
+    name: "Clara",
+    rarity: 5,
+    id: 21,
+    src: CharacterIcon21,
+    type: Physical,
+    path: "Destruction",
+    alt: "Character_Clara_Icon",
+    typeAlt: "Class_Type_Physical",
+  },
+  {
+    name: "Fu Xuan",
+    rarity: 5,
+    id: 22,
+    src: CharacterIcon22,
+    type: Quantum,
+    path: "Preservation",
+    alt: "Character_Fu_Xuan_Icon",
+    typeAlt: "Class_Type_Quantum",
+  },
+  {
+    name: "Gepard",
+    rarity: 5,
+    id: 23,
+    src: CharacterIcon23,
+    type: Ice,
+    path: "Preservation",
+    alt: "Character_Gepard_Icon",
+    typeAlt: "Class_Type_Ice",
+  },
+  {
+    name: "Himeko",
+    rarity: 5,
+    id: 24,
+    src: CharacterIcon24,
+    type: Fire,
+    path: "Erudition",
+    alt: "Character_Himeko_Icon",
+    typeAlt: "Class_Type_Fire",
+  },
+  {
+    name: "Imbibitor Lunae",
+    rarity: 5,
+    id: 25,
+    src: CharacterIcon25,
+    type: Imaginary,
+    path: "Destruction",
+    alt: "Character_Imbibitor_Lunae_Icon",
+    typeAlt: "Class_Type_Imaginary",
+  },
+  {
+    name: "Jingliu",
+    rarity: 5,
+    id: 26,
+    src: CharacterIcon26,
+    type: Ice,
+    path: "Destruction",
+    alt: "Character_Jingliu_Icon",
+    typeAlt: "Class_Type_Ice",
+  },
+  {
+    name: "Jing Yuan",
+    rarity: 5,
+    id: 27,
+    src: CharacterIcon27,
+    type: Lightning,
+    path: "Erudition",
+    alt: "Character_Jing_Yuan_Icon",
+    typeAlt: "Class_Type_Lightning",
+  },
+  {
+    name: "Kafka",
+    rarity: 5,
+    id: 28,
+    src: CharacterIcon28,
+    type: Lightning,
+    path: "Nihility",
+    alt: "Character_Luocha_Icon",
+    typeAlt: "Class_Type_Lightning",
+  },
+  {
+    name: "Luocha",
+    rarity: 5,
+    id: 29,
+    src: CharacterIcon29,
+    type: Imaginary,
+    path: "Abundance",
+    alt: "Character_Luocha_Icon",
+    typeAlt: "Class_Type_Imaginary",
+  },
+  {
+    name: "Seele",
+    rarity: 5,
+    id: 30,
+    src: CharacterIcon30,
+    type: Quantum,
+    path: "Hunt",
+    alt: "Character_Seele_Icon",
+    typeAlt: "Class_Type_Quantum",
+  },
+  {
+    name: "Silver Wolf",
+    rarity: 5,
+    id: 31,
+    src: CharacterIcon31,
+    type: Quantum,
+    path: "Nihility",
+    alt: "Character_Silver_Wolf_Icon",
+    typeAlt: "Class_Type_Quantum",
+  },
+  {
+    name: "Topaz & Numby",
+    rarity: 5,
+    id: 32,
+    src: CharacterIcon32,
+    type: Fire,
+    path: "Hunt",
+    alt: "Character_Topaz_Icon",
+    typeAlt: "Class_Type_Fire",
+  },
+  {
+    name: "Trailblazer Destruction (F)",
+    rarity: 5,
+    id: 33,
+    src: CharacterIcon33,
+    type: Physical,
+    path: "Destruction",
+    alt: "Character_Trailblazer_Female_Destruction_Icon",
+    typeAlt: "Class_Type_Physical",
+  },
+  {
+    name: "Trailblazer Preservation (F)",
+    rarity: 5,
+    id: 34,
+    src: CharacterIcon34,
+    type: Fire,
+    path: "Preservation",
+    alt: "Character_Trailblazer_Female_Preservation_Icon",
+    typeAlt: "Class_Type_Fire",
+  },
+  {
+    name: "Trailblazer Destruction (M)",
+    rarity: 5,
+    id: 35,
+    src: CharacterIcon35,
+    type: Physical,
+    path: "Destruction",
+    alt: "Character_Trailblazer_Male_Destruction_Icon",
+    typeAlt: "Class_Type_Physical",
+  },
+  {
+    name: "Trailblazer Preservation (M)",
+    rarity: 5,
+    id: 36,
+    src: CharacterIcon36,
+    type: Fire,
+    path: "Preservation",
+    alt: "Character_Trailblazer_Male_Preservation_Icon",
+    typeAlt: "Class_Type_Fire",
+  },
+  {
+    name: "Welt",
+    rarity: 5,
+    id: 37,
+    src: CharacterIcon37,
+    type: Imaginary,
+    path: "Nihility",
+    alt: "Character_Welt_Icon",
+    typeAlt: "Class_Type_Imaginary",
+  },
+  {
+    name: "Yanqing",
+    rarity: 5,
+    id: 38,
+    src: CharacterIcon38,
+    type: Ice,
+    path: "Hunt",
+    alt: "Character_Yanqing_Icon",
+    typeAlt: "Class_Type_Ice",
+  },
+];
