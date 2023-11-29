5eb0ff055a5ac7a5782e8c2e9d9fd9389970d6b2
meltestera <meltestera@gmail.com>
2023-11-29 16:50:27 +0800

    updated style

--- src/pages/CharacterDetails.jsx
+++ src/pages/CharacterDetails.jsx
@@ -1,5 +1,5 @@
 // import useParams
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 
 // import useState, useEffect
 import { useState, useEffect, useRef } from "react";
@@ -22,7 +22,7 @@ const CharacterDetails = () => {
     };
 
     if (scrollRef) {
-      window.scrollTo(0, scrollRef.current.offsetTop);
+      window.scrollTo(0, scrollRef.current.offsetTop - 20);
     }
 
     fetchData();
@@ -46,6 +46,20 @@ const CharacterDetails = () => {
                   return (
                     <div key={character.id}>
                       <div className="text-center">
+                        <Link to="/">
+                          <span>
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              height="24px"
+                              viewBox="0 0 24 24"
+                              width="24px"
+                              fill="#eee"
+                            >
+                              <path d="M0 0h24v24H0V0z" fill="none" />
+                              <path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z" />
+                            </svg>
+                          </span>
+                        </Link>
                         <span className="text-4xl font-bold">
                           {" "}
                           {character.name}
